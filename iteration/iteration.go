0ff101b8848449192ea6f9ac7ed9e657bbd7ffc1
Jordan Shaw <jordan.m.shaw@bjss.com>
2022-06-20 14:05:49 +0100

    Refactor repeat

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,9 +1,11 @@
 package iteration
 
+const repeatCount = 5
+
 func Repeat(c string) string {
 	var s string
-	for i := 0; i < 5; i++ {
-		s = s + c
+	for i := 0; i < repeatCount; i++ {
+		s += c
 	}
 
 	return s
