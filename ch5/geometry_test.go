f9cc449b5f44a5278024615f300b92c5cfb216c8
Jordan Shaw <jordan.m.shaw@bjss.com>
2022-06-20 16:45:30 +0100

    Add triangle and refactor

--- ch5/geometry_test.go
+++ ch5/geometry_test.go
@@ -22,18 +22,22 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 	areaTests := []struct {
-		shape ch5.Shape
-		want  float64
+		name    string
+		shape   ch5.Shape
+		hasArea float64
 	}{
-		{ch5.Rectangle{12, 6}, 72.0},
-		{ch5.Circle{10}, 314.1592653589793},
+		{name: "Rectangle", shape: ch5.Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+		{name: "Circle", shape: ch5.Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: ch5.Triangle{Base: 12, Height: 6}, hasArea: 36.0},
 	}
 
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
-		if got != tt.want {
-			t.Errorf("got %g want %g", got, tt.want)
-		}
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
 	}
 
 	// checkArea := func(t testing.TB, shape ch5.Shape, want float64) {
