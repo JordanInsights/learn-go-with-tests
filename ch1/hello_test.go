1ef08a307b0ee961ecd8db90d7b02e7e1aaf04e7
Jordan Shaw <jordan.m.shaw@bjss.com>
2022-06-20 13:20:06 +0100

    Refactor TestHello

--- ch1/hello_test.go
+++ ch1/hello_test.go
@@ -6,21 +6,24 @@ import (
 )
 
 func TestHello(t *testing.T) {
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
 	t.Run("saying hello to people", func(t *testing.T) {
 		got := ch1.Hello("Jordan")
 		want := "Hello, Jordan"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
-	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+	t.Run("emptry string defaults to 'World'", func(t *testing.T) {
 		got := ch1.Hello("")
 		want := "Hello, World"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 }
