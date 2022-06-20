9f26113bfd3216bf3657d8b73ae6b0bb587aa9f0
Jordan Shaw <jordan.m.shaw@bjss.com>
2022-06-20 14:27:43 +0100

    Add benchmark, example, and repeatCount parameter to iteration work

--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -1,21 +1,28 @@
 package iteration_test
 
 import (
+	"fmt"
 	"learn-go-with-tests/iteration"
 	"testing"
 )
 
 func TestRepeat(t *testing.T) {
-	repeated := iteration.Repeat("a")
-	expected := "aaaaa"
+	repeated := iteration.Repeat("a", 3)
+	expected := "aaa"
 
 	if repeated != expected {
 		t.Errorf("expected %q but got %q", expected, repeated)
 	}
 }
 
-func BenchmarkRepear(b *testing.B) {
+func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		iteration.Repeat("a")
+		iteration.Repeat("a", 3)
 	}
+}
+
+func ExampleRepeat() {
+	s := iteration.Repeat("a", 5)
+	fmt.Println(s)
+	// Output: aaaaa
 }
