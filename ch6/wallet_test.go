f0d00ba4aba55853dccb2e6aec247576cf6c75f3
Jordan Shaw <jordan.m.shaw@bjss.com>
2022-06-20 17:15:42 +0100

    Add Withdraw functionality and refactor tests

--- ch6/wallet_test.go
+++ ch6/wallet_test.go
@@ -6,14 +6,28 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	wallet := ch6.Wallet{}
-	wallet.Deposit(ch6.Bitcoin(10))
-
-	got := wallet.Balance()
-
-	want := ch6.Bitcoin(10)
-
-	if got != want {
-		t.Errorf("got %s want %s", got, want)
+	assertBalance := func(t testing.TB, wallet ch6.Wallet, want ch6.Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
 	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := ch6.Wallet{}
+		wallet.Deposit(ch6.Bitcoin(10))
+		want := ch6.Bitcoin(10)
+
+		assertBalance(t, wallet, want)
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := ch6.Wallet{}
+		wallet.Deposit(10)
+		wallet.Withdraw(5)
+
+		want := ch6.Bitcoin(5)
+		assertBalance(t, wallet, want)
+	})
 }
