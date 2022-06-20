900b307d5840bd6b881c19dc95da74f6db15f6d8
Jordan Shaw <jordan.m.shaw@bjss.com>
2022-06-20 16:37:05 +0100

    Refactor TestArea to use Table Driven Tests

--- ch5/geometry_test.go
+++ ch5/geometry_test.go
@@ -21,22 +21,36 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
+	areaTests := []struct {
+		shape ch5.Shape
+		want  float64
+	}{
+		{ch5.Rectangle{12, 6}, 72.0},
+		{ch5.Circle{10}, 314.1592653589793},
+	}
 
-	checkArea := func(t testing.TB, shape ch5.Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
 		}
 	}
 
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := ch5.Rectangle{5.0, 10.0}
-		checkArea(t, rectangle, 50.0)
-	})
+	// checkArea := func(t testing.TB, shape ch5.Shape, want float64) {
+	// 	t.Helper()
+	// 	got := shape.Area()
+	// 	if got != want {
+	// 		t.Errorf("got %g want %g", got, want)
+	// 	}
+	// }
 
-	t.Run("circles", func(t *testing.T) {
-		circle := ch5.Circle{10}
-		checkArea(t, circle, 314.1592653589793)
-	})
+	// t.Run("rectangles", func(t *testing.T) {
+	// 	rectangle := ch5.Rectangle{5.0, 10.0}
+	// 	checkArea(t, rectangle, 50.0)
+	// })
+
+	// t.Run("circles", func(t *testing.T) {
+	// 	circle := ch5.Circle{10}
+	// 	checkArea(t, circle, 314.1592653589793)
+	// })
 }
