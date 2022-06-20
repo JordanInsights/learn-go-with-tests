3b06632250285f46be9a139cb93309a3a157a312
Jordan Shaw <jordan.m.shaw@bjss.com>
2022-06-20 16:27:53 +0100

    Refactor geometry area functions to be methods on rectangle and circle types

--- ch5/geometry_test.go
+++ ch5/geometry_test.go
@@ -5,22 +5,36 @@ import (
 	"testing"
 )
 
-func assertEquality(t *testing.T, got, want float64) {
+func assert2fEquality(t *testing.T, got, want float64) {
 	if got != want {
 		t.Errorf("got %.2f want %.2f", got, want)
 	}
 }
 
 func TestPerimeter(t *testing.T) {
-	got := ch5.Perimeter(5.0, 10.0)
+	rectangle := ch5.Rectangle{5.0, 10.0}
+
+	got := ch5.Perimeter(rectangle)
 	want := 30.0
 
-	assertEquality(t, got, want)
+	assert2fEquality(t, got, want)
 }
 
 func TestArea(t *testing.T) {
-	got := ch5.Area(5.0, 10.0)
-	want := 50.0
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := ch5.Rectangle{5.0, 10.0}
+		got := rectangle.Area()
+		want := 50.0
 
-	assertEquality(t, got, want)
+		assert2fEquality(t, got, want)
+	})
+	t.Run("circles", func(t *testing.T) {
+		circle := ch5.Circle{10}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
 }
