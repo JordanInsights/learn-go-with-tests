ec13ed6abe5925a623bb2924db60cf2382cc4baf
Jordan Shaw <jordan.m.shaw@bjss.com>
2022-06-20 13:15:46 +0100

    Add second test to hello_test/TestHello

--- ch1/hello_test.go
+++ ch1/hello_test.go
@@ -6,10 +6,21 @@ import (
 )
 
 func TestHello(t *testing.T) {
-	got := ch1.Hello("Jordan")
-	want := "Hello, Jordan"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := ch1.Hello("Jordan")
+		want := "Hello, Jordan"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := ch1.Hello("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
